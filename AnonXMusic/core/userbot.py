50b60e067746762fd7a9a0151c3e090bcd63f4aa
Moalmortagel <105941198+Almortage@users.noreply.github.com>
2024-04-26 02:13:45 +0300

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,13 +51,13 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
+                await self.one.join_chat("AlmortagelTech")
                 await self.one.join_chat("FallenAssociation")
             except:
                 pass
             assistants.append(1)
             try:
-                await self.one.send_message(config.LOGGER_ID, "تم تشغيل  البوت علي سورس ميوزك فرعون 🥺💕")
+                await self.one.send_message(config.LOGGER_ID, "تم تشغيل  البوت علي سورس ميوزك المرتجل 🥺💕")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
@@ -67,18 +67,18 @@ class Userbot(Client):
             self.one.name = self.one.me.mention
             self.one.username = self.one.me.username
             assistantids.append(self.one.id)
-            LOGGER(__name__).info(f"تم تشغيل  البوت علي سورس ميوزك فرعون 🥺💕 as {self.one.name}")
+            LOGGER(__name__).info(f"تم تشغيل  البوت علي سورس ميوزك المرتجل 🥺💕 as {self.one.name}")
 
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
+                await self.two.join_chat("AlmortagelTech")
                 await self.two.join_chat("FallenAssociation")
             except:
                 pass
             assistants.append(2)
             try:
-                await self.two.send_message(config.LOGGER_ID, "تم تشغيل  البوت علي سورس ميوزك فرعون 🥺💕")
+                await self.two.send_message(config.LOGGER_ID, "تم تشغيل  البوت علي سورس ميوزك المرتجل 🥺💕")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
@@ -93,13 +93,13 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("DevilsHeavenMF")
+                await self.three.join_chat("AlmortagelTech")
                 await self.three.join_chat("FallenAssociation")
             except:
                 pass
             assistants.append(3)
             try:
-                await self.three.send_message(config.LOGGER_ID, "تم تشغيل  البوت علي سورس ميوزك فرعون 🥺💕")
+                await self.three.send_message(config.LOGGER_ID, "تم تشغيل  البوت علي سورس ميوزك المرتجل 🥺💕")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
@@ -114,13 +114,13 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("DevilsHeavenMF")
+                await self.four.join_chat("AlmortagelTech")
                 await self.four.join_chat("FallenAssociation")
             except:
                 pass
             assistants.append(4)
             try:
-                await self.four.send_message(config.LOGGER_ID, "تم تشغيل  البوت علي سورس ميوزك فرعون 🥺💕")
+                await self.four.send_message(config.LOGGER_ID, "تم تشغيل  البوت علي سورس ميوزك المرتجل 🥺💕")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
@@ -135,13 +135,13 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("DevilsHeavenMF")
+                await self.five.join_chat("AlmortagelTech")
                 await self.five.join_chat("FallenAssociation")
             except:
                 pass
             assistants.append(5)
             try:
-                await self.five.send_message(config.LOGGER_ID, "تم تشغيل  البوت علي سورس ميوزك فرعون 🥺💕")
+                await self.five.send_message(config.LOGGER_ID, "تم تشغيل  البوت علي سورس ميوزك المرتجل 🥺💕")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "

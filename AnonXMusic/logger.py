4e9410836b8c7b37167e778fc23db8cf1b3c91f8
terexip <gyy028433@gmail.com>
2024-10-22 16:15:38 +0200

    logger.py

---
+++ AnonXMusic/logger.py
@@ -0,0 +1,19 @@
+import logger
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        logging.FileHandler("log.txt"),
+        logging.StreamHandler(),
+    ],
+)
+
+logger.getLogger("httpx").setLevel(logger.ERROR)
+logger.getLogger("pyrogram").setLevel(logger.ERROR)
+logger.getLogger("pytgcalls").setLevel(logger.ERROR)
+
+
+def LOGGER(name: str) -> logger.Logger:
+    return logger.getLogger(name)

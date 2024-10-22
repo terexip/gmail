b2b5a5fdd61afacbb97c74189bf325f61cebb7a4
terexip <gyy028433@gmail.com>
2024-10-22 23:00:21 +0200

    Update __main__.py

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -1,5 +1,8 @@
 import asyncio
 import importlib
+import os
+import time
+import datetime
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
@@ -14,6 +17,11 @@ from config import BANNED_USERS
 
 
 async def init():
+    # Synchronize time before starting the bot
+    os.environ['TZ'] = 'UTC'  # يمكنك تغيير UTC إلى منطقتك الزمنية إذا لزم الأمر
+    time.tzset()
+    print("Current time:", datetime.datetime.now())  # طباعة الوقت الحالي للتحقق
+
     if (
         not config.STRING1
         and not config.STRING2
@@ -23,6 +31,7 @@ async def init():
     ):
         LOGGER(__name__).error("Assistant client variables not defined, exiting...")
         exit()
+
     await sudo()
     try:
         users = await get_gbanned()
@@ -33,25 +42,30 @@ async def init():
             BANNED_USERS.add(user_id)
     except:
         pass
+
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("AnonXMusic.plugins" + all_module)
     LOGGER("AnonXMusic.plugins").info("Successfully Imported Modules...")
+    
     await userbot.start()
     await Anony.start()
+    
     try:
         await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("AnonXMusic").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group/channel.\n\nStopping Bot..."
         )
         exit()
     except:
         pass
+    
     await Anony.decorators()
     LOGGER("AnonXMusic").info(
         "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
     )
+    
     await idle()
     await app.stop()
     await userbot.stop()

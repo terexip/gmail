629ba82c8616464ae7709fcab284bada67a5ed10
terexip <gyy028433@gmail.com>
2024-10-22 17:00:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH","f780b6a5873656b62d2c5a9f8be50bdc")
 BOT_TOKEN = getenv("BOT_TOKEN","5966375685:AAE4ivL6HbVQII4ml5AleUCAStiSwHk9wuA")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://leaboteam:26v1HFg8p6Gml2HN@cluster0.1fx9vkz.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://apptmvipbot:wV9KQkaySlqNvfQk@cluster0.y1yqp.mongodb.net/")
 
 Almortagel = getenv("Almortagel","wofkc")
 
